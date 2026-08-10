14084de003fe1d71ab4fee00c169dd6ff396156c
Annanya-Jain1410 <anniee0752@gmail.com>
2026-08-10 22:02:39 +0530

    chapter 3 list exercise

---
+++ chapter_3_lists/names.py
@@ -0,0 +1,13 @@
+names=["Anshi","Himanshi","Shreya","Drashya"]
+#printing all elements one by one through index method
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+#printing a message to all the names in the list 'names'
+print()
+print("Hii guyszzz !!")
+print(f" Hii,{names[0]}. How you doing?")
+print(f" Hii,{names[1]}. How you doing?")
+print(f" Hii,{names[2]}. How you doing?")
+print(f" Hii,{names[3]}. How you doing?")
